a54b3a78d1274ec6d61e09c8c34da0c81edcd48d
Den <denis.protva@mail.ru>
2019-03-11 16:25:23 +0300

    Update ClkDiv.v

--- ClkDevider/ClkDiv.v
+++ ClkDevider/ClkDiv.v
@@ -1,26 +1,27 @@
 module ClkDiv
- 
 (
 input clk,
 output clk_out,
 input [7:0] div
 );
 
-wire isOdd = div & 8'h01;
+wire isOdd = div & 8'h01; // there are two separate algorithm for odd and even numbers
 
-wire [7:0] N;
-assign N = div;
+wire [7:0] oN; // for odd algorithm
+assign oN = div;
 reg [7:0] pos_count, neg_count;
 
 wire [7:0] eN;
-assign eN = div>>1;
+assign eN = div>>1; // for even algorithm
 reg [7:0] r_reg;
 wire [7:0] r_nxt;
 reg clk_track;
+
 assign r_nxt = r_reg+8'h01;  
+
 always @(posedge clk) begin
 	if(isOdd)begin // optional, reduces the noise
-		if (pos_count == N-1)
+		if (pos_count == oN-1)
 			pos_count <= 0;
 		else
 			pos_count<= pos_count + 8'h01;
@@ -37,11 +38,11 @@ end
 
 always @(negedge clk)
 	if(isOdd) begin // optional, reduces the noise
-		if (neg_count == N-1)
+		if (neg_count == oN-1)
 			neg_count <= 0;
 		else
 			neg_count<= neg_count + 8'h01;
 	end
 
-assign clk_out = (((pos_count > (N>>1)) | (neg_count > (N>>1))) & isOdd) || ( (clk_track) &(~isOdd) ); 
+assign clk_out = (((pos_count > (oN>>1)) | (neg_count > (oN>>1))) & isOdd) || ( (clk_track) &(~isOdd) ); 
 endmodule
