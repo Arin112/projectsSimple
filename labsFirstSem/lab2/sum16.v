b80b76f186ae34c94bee1a923cf435a055076eb8
Den <denis.protva@mail.ru>
2019-03-13 20:32:17 +0300

    Добавлены все лабы за первый семестр

---
+++ labsFirstSem/lab2/sum16.v
@@ -0,0 +1,20 @@
+﻿module sum16 // то же самое, что и sum4, но в sum4 в качестве аргумента передаются диапазоны
+(
+input [15:0] a,
+input [15:0] b,
+input cin,
+output [15:0] c,
+output cout
+);
+wire [3:0] couts;
+sum4 inst0(.a(a[3:0]), .b(b[3:0]), .cin(cin), .c(c[3:0]), .cout(couts[0]));
+
+genvar x;
+generate
+	for(x = 1; x <4; x = x + 1)begin : _gen1
+		sum4 insts(.a(a[x*4+3:x*4]), .b(b[x*4+3:x*4]), .c(c[x*4+3:x*4]), .cin(couts[x-1]), .cout(couts[x]));
+	end
+endgenerate
+assign cout = couts[3];
+
+endmodule
---
+++ labsFirstSem/lab3/vMultiplexer.v
@@ -0,0 +1,46 @@
+﻿module vMultiplexer
+(
+input [3:0] coord, // координата - который из входных сигналов выбрать
+// по требованию задания у модуля должно быть 16 восьмибитных входных сигналов
+input [7:0] q1,
+input [7:0] q2,
+input [7:0] q3,
+input [7:0] q4,
+input [7:0] q5,
+input [7:0] q6,
+input [7:0] q7,
+input [7:0] q8,
+input [7:0] q9,
+input [7:0] q10,
+input [7:0] q11,
+input [7:0] q12,
+input [7:0] q13,
+input [7:0] q14,
+input [7:0] q15,
+input [7:0] q16,
+output [7:0] q
+);
+
+wire [7:0] qs [15:0];
+
+assign qs[0] = q1;
+assign qs[1] = q2;
+assign qs[2] = q3;
+assign qs[3] = q4;
+assign qs[4] = q5;
+assign qs[5] = q6;
+assign qs[6] = q7;
+assign qs[7] = q8;
+assign qs[8] = q9;
+assign qs[9] = q10;
+assign qs[10] = q11;
+assign qs[11] = q12;
+assign qs[12] = q13;
+assign qs[13] = q14;
+assign qs[14] = q15;
+assign qs[15] = q16;
+
+assign q = qs[coord];
+
+endmodule 
+            
